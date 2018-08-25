18360c7b6756cd5e6be07cb6ca8bcbdf138d8f0a
Proxser <kuromukura@mail.ru>
2018-08-25 15:37:48 +0700

    .

--- script.js
+++ script.js
@@ -5,21 +5,21 @@ function getCountDayOfMonth(year, month) {
 	return D.getDate();
 }
 
-// Функция для получения номера дня недели первого дня заданного месяца
+// Функция для получения номера дня недели первого дня заданного месяца (0-6)
 function getNumFirstDayOfMonth(year, month) {
 	var D = new Date(year, month, 1);
 	return D.getDay();
 }
 
-// Функция для получения номера дня недели последнего заданного месяца
+// Функция для получения номера дня недели последнего заданного месяца (0-6)
 function getNumLastDayOfMonth(year, month) {
 	var D = new Date(year, month, getCountDayOfMonth(year, month));
 	return D.getDay();
 }
 
 // Получаем календарь
-function getCalendar() {
-	var D = new Date(/*2018, 6, 1*/);
+function getCalendar(D) {
+	var /*D = new Date();*/
 		// Получаем текущую дату
 		countDayOfMonth 	= getCountDayOfMonth(D.getFullYear(), D.getMonth()),
 		// Получаем кол-во дней в месяце
@@ -31,7 +31,9 @@ function getCalendar() {
 		// Получаем номер дня недели последнего дня месяца
 
 		namesOfMonths = ["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"],
-		calendar = '<tr>';
+		calendar = '<tr>',
+		counter = 42;
+		// Счётчик равный 42 для того чтобы построить календарь размером (7х6)
 
 		console.log(D);
 		console.log(countDayOfMonth);
@@ -42,19 +44,41 @@ function getCalendar() {
 		// Распечатываем дни предыдущего месяца
 
 		if (numFirstDayOfMonth != 0) {
-			// Если 1-й день текущего месяца не воскресенье, то распечатываем
-			// дни предыдущего месяца до первого дня текущего
-			countDayOfPrevMonth -= numFirstDayOfMonth - 1;
-			for (var i = 1; i < numFirstDayOfMonth; i++) {
-				countDayOfPrevMonth++;
-				calendar += '<td class="text-muted">' + countDayOfPrevMonth + '</td>';
+		// Если 1-й день текущего месяца не воскресенье, то распечатываем
+		// дни предыдущего месяца до первого дня текущего
+			if (numFirstDayOfMonth == 1) {
+
+				console.log('Вход в цикл совершен!');
+
+				countDayOfPrevMonth -= 7;
+				for (var i = 0; i < 7; i++) {
+					countDayOfPrevMonth++;
+					calendar += '<td class="bg-light text-muted">' + countDayOfPrevMonth + '</td>';
+					counter--;
+				}
+				calendar += '</tr><tr>';
+
+			} else {
+				countDayOfPrevMonth -= numFirstDayOfMonth - 1;
+			// Отнимаем от кол-ва дней прошлого месяца нужное нам кол-во дней,
+			// чтобы построить первую строчку календаря с днями пред. месяца
+				for (var i = 1; i < numFirstDayOfMonth; i++) {
+					countDayOfPrevMonth++;
+					calendar += '<td class="bg-light text-muted">' + countDayOfPrevMonth + '</td>';
+					counter--;
+				}
 			}
+
 		} else {
+		// Иначе, если 1-й день текущего месяца - воскресенье, то распечатываем
+		// дни предыдущего месяца до первого дня текущего и дни текущего
 			countDayOfPrevMonth -= 6;
 			for (var i = 0; i < 6; i++) {
 				countDayOfPrevMonth++;
-				calendar += '<td class="text-muted">' + countDayOfPrevMonth + '</td>';
+				calendar += '<td class="bg-light text-muted">' + countDayOfPrevMonth + '</td>';
+				counter--;
 			}
+
 		}
 
 		// Распечатываем дни текущего месяца
@@ -63,8 +87,10 @@ function getCalendar() {
 
 			if (i != D.getDate()) {
 				calendar += '<td>' + i + '</td>';
+				counter--;
 			} else {
-				calendar += '<td id="today">' + i + '</td>';  // сегодняшней дате можно задать стиль CSS
+				calendar += '<td id="today" class="bg-info text-white">' + i + '</td>';
+				counter--;  // сегодняшней дате можно задать стиль CSS
 			}
 
 			if (new Date(D.getFullYear(), D.getMonth(), i).getDay() == 0) {  // если день выпадает на воскресенье, то перевод строки
@@ -74,50 +100,37 @@ function getCalendar() {
 
 		// Распечатываем дни след. месяца
 
-		if (numLastDayOfMonth != 0) {
-			for(var  i = numLastDayOfMonth, j = 1; i < 7; i++) {
-				calendar += '<td class="text-muted">' + j++ + '</td>';
+		for(var  i = 0, j = 1; i < counter; counter--) {
+			calendar += '<td class="bg-light text-muted">' + j++ + '</td>';
+			if ((counter - 1) % 7 == 0) {
+				calendar += '<tr>';
 			}
 		}
 
 		calendar += '</tr>';
 
 		console.log(calendar);
+		console.log(counter);
+		document.querySelector('#calendar tbody').innerHTML = calendar;
+		document.getElementById('month-name').innerHTML = '<h4 class="text-white">' + 
+														  namesOfMonths[D.getMonth()] + 
+														  ' ' + 
+														  D.getFullYear() + '</h4>';
+		
 }
 
-getCalendar();
+function getPrevMonth(D){
+	D.setMonth(D.getMonth() - 1);
+	getCalendar(D);
+}
 
-// var D1 = new Date(),
-//     D1last = new Date(D1.getFullYear(),D1.getMonth()+1,0).getDate(), // последний день месяца
-//     D1Nlast = new Date(D1.getFullYear(),D1.getMonth(),D1last).getDay(), // день недели последнего дня месяца
-//     D1Nfirst = new Date(D1.getFullYear(),D1.getMonth(),1).getDay(), // день недели первого дня месяца
-//     calendar1 = '<tr>',
-//     month=["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"]; // название месяца, вместо цифр 0-11
+function getNextMonth(D){
+	D.setMonth(D.getMonth() + 1);
+	getCalendar(D);
+}
 
-// // пустые клетки до первого дня текущего месяца
-// if (D1Nfirst != 0) {
-//   for(var  i = 1; i < D1Nfirst; i++) calendar1 += '<td>';
-// }else{ // если первый день месяца выпадает на воскресенье, то требуется 7 пустых клеток 
-//   for(var  i = 0; i < 6; i++) calendar1 += '<td>';
-// }
+var D = new Date(2018, 0, 1);
 
-// // дни месяца
-// for(var  i = 1; i <= D1last; i++) {
-//   if (i != D1.getDate()) {
-//     calendar1 += '<td>' + i;
-//   }else{
-//     calendar1 += '<td id="today">' + i;  // сегодняшней дате можно задать стиль CSS
-//   }
-//   if (new Date(D1.getFullYear(),D1.getMonth(),i).getDay() == 0) {  // если день выпадает на воскресенье, то перевод строки
-//     calendar1 += '<tr>';
-//   }
-// }
-
-// // пустые клетки после последнего дня месяца
-// if (D1Nlast != 0) {
-//   for(var  i = D1Nlast; i < 7; i++) calendar1 += '<td>';
-// }
-
-// document.querySelector('#calendar1 tbody').innerHTML = calendar1;
-// document.querySelector('#calendar1 thead td:last-child').innerHTML = D1.getFullYear();
-// document.querySelector('#calendar1 thead td:first-child').innerHTML = month[D1.getMonth()];
+getCalendar(D);
+document.getElementById('#prevMonth').onclick = getCalendar(D.setMonth(D.getMonth() - 1));
+document.querySelector('#nextMonth').onclick = getCalendar(D.setMonth(D.getMonth() + 1));
