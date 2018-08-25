41e1bc8b2990b5f18a4f60ff424b2816da942f2c
Proxser <kuromukura@mail.ru>
2018-08-26 00:26:57 +0700

    .

--- script.js
+++ script.js
@@ -18,50 +18,66 @@ function getNumLastDayOfMonth(year, month) {
 }
 
 // Получаем календарь
-function getCalendar(D) {
-	var /*D = new Date();*/
-		// Получаем текущую дату
-		countDayOfMonth 	= getCountDayOfMonth(D.getFullYear(), D.getMonth()),
-		// Получаем кол-во дней в месяце
-		countDayOfPrevMonth = getCountDayOfMonth(D.getFullYear(), D.getMonth() - 1),
-		// Получаем кол-во дней в предыдущем месяце
-		numFirstDayOfMonth 	= getNumFirstDayOfMonth(D.getFullYear(), D.getMonth()),
-		// Получаем номер дня недели первого дня месяца
-		numLastDayOfMonth 	= getNumLastDayOfMonth(D.getFullYear(), D.getMonth()),
-		// Получаем номер дня недели последнего дня месяца
+function getCalendar(id, D) {
 
-		namesOfMonths = ["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"],
-		calendar = '<tr>',
-		counter = 42;
+	// -------------------------------------------------------------------------
+	// Объявление переменных
+	// -------------------------------------------------------------------------
+
+	var countDayOfMonth 	= getCountDayOfMonth(D.getFullYear(), D.getMonth()),
+		// Получаем кол-во дней в месяце (1-31)
+
+		countDayOfPrevMonth = getCountDayOfMonth(D.getFullYear(), D.getMonth() - 1),
+		// Получаем кол-во дней в предыдущем месяце (1-31)
+
+		numFirstDayOfMonth 	= getNumFirstDayOfMonth(D.getFullYear(), D.getMonth()),
+		// Получаем номер дня недели первого дня месяца (0-6)
+
+		numLastDayOfMonth 	= getNumLastDayOfMonth(D.getFullYear(), D.getMonth()),
+		// Получаем номер дня недели последнего дня месяца (0-6)
+
+		namesOfMonths 		= ["Январь", "Февраль", "Март", "Апрель", "Май",
+							   "Июнь", "Июль", "Август", "Сентябрь",
+							   "Октябрь", "Ноябрь", "Декабрь"],
+		// Массив с названиями месяцев для отображения надписи
+
+		calendar      		= '<tr>',
+		// Переменная в которую в конце будет сформирован весь календарь
+
+		counter       		= 42;
 		// Счётчик равный 42 для того чтобы построить календарь размером (7х6)
 
-		console.log(D);
-		console.log(countDayOfMonth);
-		console.log(countDayOfPrevMonth);
-		console.log(numFirstDayOfMonth);
-		console.log(numLastDayOfMonth);
 
-		// Распечатываем дни предыдущего месяца
+		// ---------------------------------------------------------------------
+		// Создаём ячейки с днями предыдущего месяца
+		// ---------------------------------------------------------------------
+
+		// Если 1-й день текущего месяца не воскресенье, то печатаем дни
+		// предыдущего месяца
 
 		if (numFirstDayOfMonth != 0) {
-		// Если 1-й день текущего месяца не воскресенье, то распечатываем
-		// дни предыдущего месяца до первого дня текущего
+		
+		// Если 1-й день текущего месяца начинается с понедельника, то всю 1-ю 
+		// строчку календаря заполняем днями предыдущего месяца, иначе печатаем 
+		// необходимое кол-во дней предыдущего месяца, чтобы заполнить
+
 			if (numFirstDayOfMonth == 1) {
-
-				console.log('Вход в цикл совершен!');
-
+		
 				countDayOfPrevMonth -= 7;
 				for (var i = 0; i < 7; i++) {
 					countDayOfPrevMonth++;
 					calendar += '<td class="bg-light text-muted">' + countDayOfPrevMonth + '</td>';
 					counter--;
 				}
+
 				calendar += '</tr><tr>';
 
 			} else {
+
+		// Узнаем необходимое кол-во дней пр
+
 				countDayOfPrevMonth -= numFirstDayOfMonth - 1;
-			// Отнимаем от кол-ва дней прошлого месяца нужное нам кол-во дней,
-			// чтобы построить первую строчку календаря с днями пред. месяца
+
 				for (var i = 1; i < numFirstDayOfMonth; i++) {
 					countDayOfPrevMonth++;
 					calendar += '<td class="bg-light text-muted">' + countDayOfPrevMonth + '</td>';
@@ -73,64 +89,69 @@ function getCalendar(D) {
 		// Иначе, если 1-й день текущего месяца - воскресенье, то распечатываем
 		// дни предыдущего месяца до первого дня текущего и дни текущего
 			countDayOfPrevMonth -= 6;
+
 			for (var i = 0; i < 6; i++) {
 				countDayOfPrevMonth++;
 				calendar += '<td class="bg-light text-muted">' + countDayOfPrevMonth + '</td>';
 				counter--;
 			}
-
 		}
 
-		// Распечатываем дни текущего месяца
+
+		// ---------------------------------------------------------------------
+		// Создаём ячейки с днями текущего месяца
+		// ---------------------------------------------------------------------
 
 		for (var  i = 1; i <= countDayOfMonth; i++) {
-
 			if (i != D.getDate()) {
+
 				calendar += '<td>' + i + '</td>';
 				counter--;
+
 			} else {
+
 				calendar += '<td id="today" class="bg-info text-white">' + i + '</td>';
-				counter--;  // сегодняшней дате можно задать стиль CSS
+				counter--;
+
 			}
 
-			if (new Date(D.getFullYear(), D.getMonth(), i).getDay() == 0) {  // если день выпадает на воскресенье, то перевод строки
+			if (new Date(D.getFullYear(), D.getMonth(), i).getDay() == 0) {
+			// если день выпадает на воскресенье, то перевод строки
 		    	calendar += '</tr><tr>';
 			}
 		}
 
-		// Распечатываем дни след. месяца
 
-		for(var  i = 0, j = 1; i < counter; counter--) {
+		// ---------------------------------------------------------------------
+		// Создаём ячейки с днями следующего месяца
+		// ---------------------------------------------------------------------
+
+		for (var  i = 0, j = 1; i < counter; counter--) {
 			calendar += '<td class="bg-light text-muted">' + j++ + '</td>';
-			if ((counter - 1) % 7 == 0) {
+
+			if ( (counter - 1) % 7 == 0 ) {
 				calendar += '<tr>';
 			}
 		}
 
 		calendar += '</tr>';
 
-		console.log(calendar);
-		console.log(counter);
-		document.querySelector('#calendar tbody').innerHTML = calendar;
-		document.getElementById('month-name').innerHTML = '<h4 class="text-white">' + 
-														  namesOfMonths[D.getMonth()] + 
-														  ' ' + 
-														  D.getFullYear() + '</h4>';
-		
+		$('#' + id + ' tbody').html(calendar);
+		$('#month-name').html(
+			'<h4 class="text-white">' +
+				namesOfMonths[D.getMonth()] + ' ' + D.getFullYear() +
+			'</h4>');
+		$('#month-name').data('month', D.getMonth());
+		$('#month-name').data('year', D.getFullYear());	
 }
 
-function getPrevMonth(D){
-	D.setMonth(D.getMonth() - 1);
-	getCalendar(D);
-}
+var D = new Date();
 
-function getNextMonth(D){
-	D.setMonth(D.getMonth() + 1);
-	getCalendar(D);
-}
+getCalendar('calendar', D);
 
-var D = new Date(2018, 0, 1);
-
-getCalendar(D);
-document.getElementById('#prevMonth').onclick = getCalendar(D.setMonth(D.getMonth() - 1));
-document.querySelector('#nextMonth').onclick = getCalendar(D.setMonth(D.getMonth() + 1));
+$('#prevMonth').click(function() {
+	getCalendar('calendar', new Date($('#month-name').data().year, $('#month-name').data().month - 1, 1));
+});
+$('#nextMonth').click(function() {
+	getCalendar('calendar', new Date($('#month-name').data().year, $('#month-name').data().month + 1, 1));
+});
